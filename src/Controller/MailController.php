37dbb85e8e5f71cb5ed399119f2390aaa3ea2fdd
ryuring <ryuring55@gmail.com>
2026-07-30 09:33:22 +0000

    baser-core の composer.json のバージョン番号を更新

--- src/Controller/MailController.php
+++ src/Controller/MailController.php
@@ -74,8 +74,8 @@ class MailController extends MailFrontAppController
      */
     public function beforeFilter(EventInterface $event)
     {
-        $redirect = parent::beforeFilter($event);
-        if($redirect) return $redirect;
+        parent::beforeFilter($event);
+        if ($event->getResult()) return;
 
         if (!$this->request->getParam('entityId')) {
             $this->notFound();
@@ -363,7 +363,7 @@ class MailController extends MailFrontAppController
      *
      * @param BcCaptchaServiceInterface $service
      * @param string $token
-     * @return void
+     * @return \Cake\Http\Response
      * @checked
      * @noTodo
      * @unitTest
@@ -371,7 +371,11 @@ class MailController extends MailFrontAppController
     public function captcha(BcCaptchaServiceInterface $service, string $token)
     {
         $this->disableAutoRender();
-        $service->render($this->getRequest(), $token);
+        $image = $service->render($this->getRequest(), $token);
+        $type = function_exists('imagejpeg') ? 'jpg' : (function_exists('imagegif') ? 'gif' : 'png');
+        return $this->getResponse()
+            ->withType($type)
+            ->withStringBody($image);
     }
 
 }
